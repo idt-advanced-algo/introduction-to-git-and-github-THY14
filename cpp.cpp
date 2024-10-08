ec3b413abc063022b7c480cf8370ca501b1bad91
methy <methymuy@gmail.com>
2024-10-08 10:50:03 +0700

    1

---
+++ cpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"aba owner";
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    cout<<"number of n is : ";
+}
